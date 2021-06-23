1ed6733f76d617ba1f6f1074a95b6490a163562c
rcuevass <rogercuevas.ca@gmail.com>
2021-06-23 00:07:48 -0400

    refactoring and extra logging

--- src/data_read_prep/data_read.py
+++ src/data_read_prep/data_read.py
@@ -1,9 +1,18 @@
+# to handle data frames
 import pandas as pd
 
 
-def read_csv_select_type(path_to_csv_file: str, data_type: str = 'number'):
+def read_csv_select_type(path_to_csv_file: str, data_type: str = 'number') -> pd.DataFrame:
+    """
+    Function that reads a csv file to a data frame and selects only certain data type
+    :param path_to_csv_file: string indicated the path to and csv file
+    :param data_type: string indicating the datatype to be kept
+                      default set to numeric
+    :return: data_frame: data frame capturing data
+    """
 
+    # read data from csv file
     data_frame = pd.read_csv(path_to_csv_file)
+    # selecting data type from function argument
     data_frame = data_frame.select_dtypes(include=data_type)
     return data_frame
-
--- src/main.py
+++ src/main.py
@@ -13,12 +13,40 @@ from sklearn.model_selection import train_test_split
 log_ = get_log_object_named('main')
 
 
-def main():
+def process_raw_data() -> None:
+    log_.info('Pre-processed data does not exist...')
+    log_.info('Reading raw data from=%s', '../data/input/archive/adult.csv')
+    df = pd.read_csv('../data/input/archive/adult.csv')
+    log_.info('Encoding categorical features via OHE...')
+    df = encode_features_ohe(['race', 'gender', 'workclass'], df)
+    log_.info('Creating labels for classifier training...')
+    df['income'] = df['income'].map({'<=50K': 0, '>50K': 1})
+
+    log_.info('splitting data to create training, test reference and test prod...')
+    df_train, df_test = train_test_split(df, test_size=0.4, random_state=2021)
+    log_.info('creating unforced test reference data...')
+    df_test_ref, df_test_prod_unforced = train_test_split(df_test, test_size=0.5, random_state=2021)
+    log_.info('creating forced test prod data...')
+    df_test_prod_forced = force_drift(df_test_prod_unforced, list_drift_num=['age'])
+
+    log_.info('saving training data to csv=%s', '../data/input/train.csv')
+    df_train.to_csv('../data/input/train.csv', index=False)
+    log_.info('saving test reference data to csv=%s', '../data/input/test_reference.csv')
+    df_test_ref.to_csv('../data/input/test_reference.csv', index=False)
+    log_.info('saving unforced prod data to csv=%s', '../data/input/test_prod_unforced.csv')
+    df_test_prod_unforced.to_csv('../data/input/test_prod_unforced.csv', index=False)
+    log_.info('saving forced prod data to csv=%s', '../data/input/test_prod_forced.csv')
+    df_test_prod_forced.to_csv('../data/input/test_prod_forced.csv', index=False)
+
+
+def train_model_data_drift() -> None:
     # training model and storing binary
     log_.info('Training model and storing binary...')
     dict_acc_auc = train_store_model(path_csv_train='../data/input/train.csv',
                                      path_csv_test='../data/input/test_reference.csv',
                                      path_name_pkl_trained='../models/knn_clf.pkl')
+
+    log_.info('Accuracy and auc on training and test datasets=%s', str(dict_acc_auc))
 
     # read reference and prod data and score on both
     log_.info('Reading reference and prod data and scoring...')
@@ -28,33 +56,19 @@ def main():
                               list_features_drift_report=['age'])
 
 
-if __name__ == '__main__':
+def main():
     log_.info('Checking if pre-processed csv datasets exist...')
     if path.exists('../data/input/train.csv') & path.exists('../data/input/test_reference.csv'):
-        log_.info('Pre-processed data exist, proceeding with data drift analysis and model training...')
-        main()
+        log_.info('Pre-processed data exists, proceeding with data drift analysis and model training...')
+        train_model_data_drift()
+        log_.info('Model has been trained and data drift report has been generated...')
     else:
-        log_.info('Pre-processed data does not exist...')
-        log_.info('Reading raw data from=%s', '../data/input/archive/adult.csv')
-        df = pd.read_csv('../data/input/archive/adult.csv')
-        log_.info('Encoding categorical features via OHE...')
-        df = encode_features_ohe(['race', 'gender', 'workclass'], df)
-        log_.info('Creating labels for classifier training...')
-        df['income'] = df['income'].map({'<=50K': 0, '>50K': 1})
+        log_.info('Pre-processed data does not exist. Starting raw data processing...')
+        process_raw_data()
+        log_.info('Raw data has been processed and is ready for model training...')
+        train_model_data_drift()
+        log_.info('Model has been trained and data drift report has been generated...')
 
-        log_.info('splitting data to create training, test reference and test prod...')
-        df_train, df_test = train_test_split(df, test_size=0.4, random_state=2021)
-        log_.info('creating unforced test reference data...')
-        df_test_ref, df_test_prod_unforced = train_test_split(df_test, test_size=0.5, random_state=2021)
-        log_.info('creating forced test prod data...')
-        df_test_prod_forced = force_drift(df_test_prod_unforced, list_drift_num=['age'])
 
-        log_.info('saving training data to csv=%s', '../data/input/train.csv')
-        df_train.to_csv('../data/input/train.csv', index=False)
-        log_.info('saving test reference data to csv=%s', '../data/input/test_reference.csv')
-        df_test_ref.to_csv('../data/input/test_reference.csv', index=False)
-        log_.info('saving unforced prod data to csv=%s', '../data/input/test_prod_unforced.csv')
-        df_test_prod_unforced.to_csv('../data/input/test_prod_unforced.csv', index=False)
-        log_.info('saving forced prod data to csv=%s', '../data/input/test_prod_forced.csv')
-        df_test_prod_forced.to_csv('../data/input/test_prod_forced.csv', index=False)
-        main()
+if __name__ == '__main__':
+    main()
